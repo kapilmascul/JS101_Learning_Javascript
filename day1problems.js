34c77e52dc0cd29c1ead053f0022b21e1517b7cf
kapilmascul <kabilanprimeeducator@gmail.com>
2022-10-12 19:23:02 +0530

    Day1

---
+++ day1problems.js
@@ -0,0 +1,17 @@
+
+
+let detectPrime=(num)=>{
+
+    let count=0;
+    for(let i=1; i<=num; i++){
+        if (num%i==0){
+         count++;
+        }
+      }
+      if(count==2){
+        console.log("PrimeNo")
+      }else{
+        console.log("Not a PrimeNo")
+      }
+    }
+    detectPrime(59);
