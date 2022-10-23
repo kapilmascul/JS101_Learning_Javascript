d795298c28c453c99b91809f7b63db0a6e921be7
tamalWeb <tamalmalgope@gmail.com>
2022-10-23 21:45:12 +0530

    correction done

--- day1problems.js
+++ day1problems.js
@@ -1,17 +1,13 @@
-
-
-let detectPrime=(num)=>{
-
-    let count=0;
-    for(let i=1; i<=num; i++){
-        if (num%i==0){
-         count++;
-        }
-      }
-      if(count==2){
-        console.log("PrimeNo")
-      }else{
-        console.log("Not a PrimeNo")
-      }
-    }
-    detectPrime(79);
+let count =0 ;
+let num=166 ;
+for (let i=1;i<=num;i++){
+  if (num%i==0){
+    count++ ;
+  }
+}
+if (count==2){
+  console.log("Yes");
+}
+else{
+  console.log("No")
+}
